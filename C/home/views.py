42e5af95ea5a29457f6ebee7e801c95579809774
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-12-04 14:01:26 +0330

    TemplateView

--- C/home/views.py
+++ C/home/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
+from .models import Car
 
 
-class Home(View):
-    http_method_names = ['get', 'post', 'options']
+class Home(TemplateView):
+    # http_method_names = ['get', 'post', 'options']
+    #
+    # def get(self, request):
+    #     return render(request, 'home/home.html')
+    #
+    # def options(self, request, *args, **kwargs):
+    #     response = super().options(request, *args, **kwargs)
+    #     response.headers['host'] = 'localhost'
+    #     response.headers['user'] = request.user
+    #     return response
+    #
+    # def http_method_not_allowed(self, request, *args, **kwargs):
+    #     return render(request, 'method_not_allowed.html')
+    template_name = 'home/home.html'
 
-    def get(self, request):
-        return render(request, 'home/home.html')
-
-    def options(self, request, *args, **kwargs):
-        response = super().options(request, *args, **kwargs)
-        response.headers['host'] = 'localhost'
-        response.headers['user'] = request.user
-        return response
-
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        return render(request, 'method_not_allowed.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cars'] = Car.objects.all()
+        return context
 
