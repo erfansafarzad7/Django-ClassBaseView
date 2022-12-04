3e3b81f39219d35cda6d012ddb550fa1406000d5
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-12-04 16:49:42 +0330

    listView

--- C/home/urls.py
+++ C/home/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('two/<int:id>/<str:name>', views.Two.as_view(), name='two'),
+    # path('two/<int:id>/<str:name>', views.Two.as_view(), name='two'),
 ]
--- C/home/views.py
+++ C/home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, RedirectView
+from django.views.generic.list import ListView
 from .models import Car
 
 
-class Home(TemplateView):
+# class Home(TemplateView):
     # http_method_names = ['get', 'post', 'options']
     #
     # def get(self, request):
@@ -18,26 +19,44 @@ class Home(TemplateView):
     #
     # def http_method_not_allowed(self, request, *args, **kwargs):
     #     return render(request, 'method_not_allowed.html')
+
+
+
+    # template_name = 'home/home.html'
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['cars'] = Car.objects.all()
+    #     return context
+
+
+class Home(ListView):
     template_name = 'home/home.html'
+    # model = Car
+    # queryset = Car.objects.filter(year__gte=2000)
+    context_object_name = 'cars'
+    # ordering = 'year'
+
+    def get_queryset(self):
+        return Car.objects.filter(year__gte=2000)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['cars'] = Car.objects.all()
+        context['username'] = 'jack'
         return context
 
-
-class Two(RedirectView):
-
-    # url = 'https://google.com'
-    # pattern_name = 'home:home'
-    #
-    # def get_redirect_url(self, *args, **kwargs):
-    #     print('='*90)
-    #     print(kwargs['id'], kwargs['name'])
-    #     return super().get_redirect_url(*args, **kwargs)
-
-    url = '/home/%(id)s/%(name)s/'
-
-    def get_redirect_url(self, *args, **kwargs):
-        print('='*90)
-        return super().get_redirect_url(*args, **kwargs)
+# class Two(RedirectView):
+#
+#     # url = 'https://google.com'
+#     # pattern_name = 'home:home'
+#     #
+#     # def get_redirect_url(self, *args, **kwargs):
+#     #     print('='*90)
+#     #     print(kwargs['id'], kwargs['name'])
+#     #     return super().get_redirect_url(*args, **kwargs)
+#
+#     url = '/home/%(id)s/%(name)s/'
+#
+#     def get_redirect_url(self, *args, **kwargs):
+#         print('='*90)
+#         return super().get_redirect_url(*args, **kwargs)
