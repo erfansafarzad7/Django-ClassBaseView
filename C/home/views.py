e78c5ec44196da56949c0c1d8a87e17671c157af
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-12-04 12:54:01 +0330

    options-methodnotallowed-methodnames

--- C/home/views.py
+++ C/home/views.py
@@ -3,5 +3,17 @@ from django.views import View
 
 
 class Home(View):
+    http_method_names = ['get', 'post', 'options']
+
     def get(self, request):
         return render(request, 'home/home.html')
+
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers['host'] = 'localhost'
+        response.headers['user'] = request.user
+        return response
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return render(request, 'method_not_allowed.html')
+
