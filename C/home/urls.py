a562b0bd575ce3001b0a2e14825d840d7556839e
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-12-05 17:22:35 +0330

    LoginView

--- C/home/urls.py
+++ C/home/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('delete/<int:pk>', views.CarDeleteView.as_view(), name='car_delete'),
-    path('update/<int:pk>', views.CarUpdateView.as_view(), name='car_update'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+
 
 ]
 
--- C/home/views.py
+++ C/home/views.py
@@ -1,7 +1,8 @@
 from .models import Car
-from django.views.generic import ListView, DeleteView, UpdateView
+from django.views.generic import ListView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth import views as auth_view
 
 
 class Home(ListView):
@@ -10,14 +11,8 @@ class Home(ListView):
     context_object_name = 'cars'
 
 
-class CarDeleteView(DeleteView):
-    model = Car
-    success_url = reverse_lazy('home:home')
-    template_name = 'home/delete.html'
+class UserLoginView(auth_view.LoginView):
+    template_name = 'home/login.html'
+    next_page = reverse_lazy('home:home')
 
-class CarUpdateView(UpdateView):
-    model = Car
-    fields = ['name', 'year']
-    success_url = reverse_lazy('home:home')
-    template_name = 'home/update.html'
 
